2c2d1744ee6b4b2cf0c3c442e83ef1ec92c0c84d
LeiYW <1902862824@qq.com>
2019-01-02 23:30:26 +0800

    '初始化'

---
+++ CouchbaseDemo/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Couchbase.Extensions.Caching;
+using Couchbase.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CouchbaseDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddCouchbase(opt =>
+            {
+                opt.Servers = new List<Uri>
+    {
+        new Uri("http://localhost:51239")
+    };
+                opt.Username = "123";
+                opt.Password = "123";
+            });
+
+            services.AddDistributedCouchbaseCache("123cache", opt => { });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
